5c5a16d115c89ee26b08e431dd67462b18e4027f
Nils Schweinsberg <mail@n-sch.de>
2010-10-05 00:37:46 +0200

    Cabalized

--- src/Network/Akismet.hs
+++ src/Network/Akismet.hs
@@ -1,13 +1,15 @@
+module Network.Akismet
+    ( verifyKey
+    ) where
+
 import Network.Browser
 import Network.HTTP
 import Network.URI
 
-{- | 
-    verifyKey simply tries to verify your API key, it should be called before every
-    other akismet related operation.
--}
-verifyKey :: String -- ^ The Akismet API key
-          -> String -- ^ The blog url
+-- | verifyKey simply tries to verify your API key, it should be called before
+-- every other akismet related operation.
+verifyKey :: String         -- ^ The Akismet API key
+          -> String         -- ^ The blog url
           -> IO Bool
 verifyKey key blog = do
     response <- simpleHTTP $ formToRequest (Form POST uri [("key", key), ("blog", blog)])
